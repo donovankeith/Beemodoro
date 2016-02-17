65fd5d47b2beadc61b78314b06cd6e3effc95573
Donovan Keith <donovanskeith@gmail.com>
2016-02-16 18:59:55 -0800

    Added some colors, etc.

--- intro.py
+++ intro.py
@@ -4,9 +4,14 @@ root = tkinter.Tk()
 
 hi_there = tkinter.Label(
     root,
-    text="Hi there!"
+    text="Hi there!",
+    bg="red",
+    fg="white"
 )
 
-hi_there.pack()
+hi_there.pack(fill=tkinter.BOTH, expand=True)
+
+my_name = tkinter.Label(root, text="My name is Donovan.")
+my_name.pack()
 
 root.mainloop()
